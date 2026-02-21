bbb31dfb6f22669fffe093a0fe0207e7f41fbbcf
Fabio Losavio <fabio.losavio@hotmail.it>
2026-02-21 19:59:54 +0100

    added dictionaries for working and slacking npcs, added revenue calculation

--- scenes/GameManager/game_manager.gd
+++ scenes/GameManager/game_manager.gd
@@ -8,7 +8,16 @@ extends Node
 var map_instance
 var spawn_positions
 
+var total_revenues: float = 0.0
+
 var names : Array[String] = ["Grizzle Profitgrub", "Snark Ledgerfang", "Boggle Spreadsheet", "Krimp Bonusclaw", "Snik KPI-Snatcher", "Murgle Coffeestain", "Zibble Paperjam", "Grint Marginchewer", "Blort Deadlinegnaw", "Skaggy Synergytooth", "Nibwick Microgrind", "Crindle Stocksniff", "Wizzle Cubiclebane", "Throg Expensefang", "Splug Overtimebelch", "Drabble Taskmangler", "Klix Compliancegrime", "Mizzle Workflowrot", "Gorp Staplechewer", "Snibble Budgetbruise", "Kraggy Meetinglurker", "Blim Forecastfumble", "Zonk Assetgnash", "Triggle Slidereviser", "Vorny Timesheetterror", "Glim Auditnibble", "Brakka Breakroomraider", "Sprock Redtapewriggler", "Nurgle Powerpointhex", "Grizzleback Clawculator", "Snaggle Metricsmash", "Plib Shareholdershriek", "Drox Inboxhoarder", "Fizzle Ladderclimb", "Krumble Deskgnarl", "Wretchy Watercoolerspy", "Blix Quarterlyquiver", "Grottin Promotionpounce", "Skibble Faxmachinebane", "Zraggy Corporatecackle"]
+var active_npcs: Dictionary[String, Node2D] = {}
+
+var working_npcs: Dictionary[String, Node2D] = {}
+var slacking_npcs: Dictionary[String, Node2D] = {}
+
+#TO CHANGE
+var NPC_REVENUES = 100
 
 func determine_spawn_positions(map_instance) -> Array[Vector2i]:
 	var tilemap =  map_instance.get_node("TileMap")
@@ -36,6 +45,10 @@ func determine_spawn_positions(map_instance) -> Array[Vector2i]:
 	return global_spawnable_positions
 	
 
+func update_revenues(delta):
+	for npc in self.working_npcs:
+		self.total_revenues += NPC_REVENUES*delta
+
 func _ready() -> void:
 	# SET RANDOMIZER
 	randomize()
@@ -49,18 +62,29 @@ func _ready() -> void:
 	
 	# SPAWN NPCs
 	for n in npc_counter:
-		print("Spawning Goblin ", names.pick_random())
+		var name = names.pick_random()
+		print("Spawning Goblin ", )
 		var new_npc = npc.instantiate()
 		
 		var random_spawn_position = spawnable_positions.pick_random()
 		spawnable_positions.erase(random_spawn_position)
 		
 		new_npc.global_position = random_spawn_position
+		new_npc.name = name
+		
+		active_npcs[name] = new_npc
+		
 		map_instance.add_child(new_npc)
 		
-		
-		
+	# SET WORKING AND SLACKING NPCs
+	working_npcs = active_npcs.duplicate()
+	slacking_npcs = {}
+	
 
+func _process(delta: float) -> void:
+	update_revenues(delta)
+		
+	$Label.text = str(roundi(total_revenues))
 		
 		
 		
